84c524692af89b1d1474a8667a62e1e68f7b8ae2
Marcospl0 <paulomarcospaulocosta@gmail.com>
2025-04-07 14:58:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot JetX ativo e pronto para enviar sinais!")
 
-if __name__ == "__main__":
+def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
+if __name__ == "__main__":
+    main()
+
